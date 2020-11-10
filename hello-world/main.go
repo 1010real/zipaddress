a6fbc2e3d6808c0236a3c8703bd94a3532fd21a6
1010real <moka0329@gmail.com>
2020-11-11 02:19:48 +0900

    implement api without search db

--- hello-world/main.go
+++ hello-world/main.go
@@ -1,51 +1,58 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"io/ioutil"
-	"net/http"
+	"encoding/json"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-var (
-	// DefaultHTTPGetAddress Default Address
-	DefaultHTTPGetAddress = "https://checkip.amazonaws.com"
-
-	// ErrNoIP No IP found in response
-	ErrNoIP = errors.New("No IP in HTTP response")
-
-	// ErrNon200Response non 200 status code in response
-	ErrNon200Response = errors.New("Non 200 Response found")
-)
-
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	resp, err := http.Get(DefaultHTTPGetAddress)
+	zipcode := request.QueryStringParameters["zipcode"]
+	if len(zipcode) != 7 	 {
+		errorResponse, _ := json.Marshal(&ErrorResponse{
+			Message: "Wrong zipcode received",
+		})
+		return events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body: string(errorResponse),
+		}, nil
+	}
+	pref := "東京都"
+	city := "品川区"
+	town := "五反田"
+	address := &AddressResponse{
+		Zipcode: zipcode,
+		Pref: pref,
+		City: city,
+		Town: town,
+	}
+
+	body, err := json.Marshal(address)
 	if err != nil {
 		return events.APIGatewayProxyResponse{}, err
-	}
-
-	if resp.StatusCode != 200 {
-		return events.APIGatewayProxyResponse{}, ErrNon200Response
-	}
-
-	ip, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
-
-	if len(ip) == 0 {
-		return events.APIGatewayProxyResponse{}, ErrNoIP
-	}
+	}   
 
 	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("Hello, %v", string(ip)),
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+		Body: string(body),
 		StatusCode: 200,
 	}, nil
 }
 
 func main() {
 	lambda.Start(handler)
+}
+
+type AddressResponse struct {
+	Zipcode string `json:"zipcode"`
+	Pref    string `json:"pref"`
+	City    string `json:"city"`
+	Town    string `json:"address"`
+}
+
+type ErrorResponse struct {
+	Message string `json:"message"`
 }
